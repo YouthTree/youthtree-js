a6b97360ba2658e0e1181684accc651df6c33d45
Darcy Laycock <sutto@sutto.net>
2010-11-28 16:35:56 +0800

    Bump version to a pre

--- lib/youthtree-js.rb
+++ lib/youthtree-js.rb
@@ -1,8 +1,13 @@
 class YouthTreeJS
-  VERSION = "0.2.0".freeze
+  VERSION = "0.3.0.pre".freeze
+
+  def self.coffeescripts_root
+    File.expand_path('../coffeescripts', File.dirname(__FILE__))
+  end
 
   def self.register_framework!
-    Barista::Framework.register 'youthtree', File.expand_path('../coffeescripts', File.dirname(__FILE__))
+    Barista::Framework.register :name => 'youthtree', 
+                                :root => coffeescripts_root
   end
 
   register_framework! if defined? Barista::Framework
